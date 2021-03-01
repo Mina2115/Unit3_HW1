2761fddcaf6ee3f7b18c1626e0fe104eda941050
Mina2115 <78247742+Mina2115@users.noreply.github.com>
2021-03-01 15:19:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
-        });
+require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+      "esri/widgets/Home",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Camera, Home) {
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 10,
-          center: [-89.85, 38.85] // longitude, latitude
-        });
+      /*var map = new Map({
+        basemap: "streets",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+         id:"8046207c1c214b5587230f5e5f8efc77" 
+        }
       });
+      var camera = new Camera ({
+       position: [
+           -71.060217,
+          42.382655,
+          2500// elevation in meters
+        ],
+        tilt:45,
+        heading: 0 
+      })
+
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+		viewingMode:'global',
+        camera: camera,
+        environment: { 
+             lighting: {
+               date: new Date(),
+               directchadowsEnabled:
+     true,
+               cameraTrackingEnabled:
+     true
+             }
+        },
+      });
+     var homeBtn = new Home({
+        view: view
+      });
+
+      // Add the home button to the top left corner of the view
+    view.ui.add(homeBtn, "top-left");
+    });
+
